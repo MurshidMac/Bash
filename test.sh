a2139fa61dcaa7b9528072506f53b8aaa185d57a
MurshidMac <mh.murshidco.uk@outlook.com>
2017-08-02 22:39:17 +0530

    file execution permission

--- test.sh
+++ test.sh
@@ -1,3 +1,7 @@
+# file execution permission should be obtain first
+# chmod +x test.sh 
+# ./test.sh
+ 
 echo abc   | # normal comment OK there
      tr a-z A-Z | #another normal comment
      sort |  # the pipelines are automatically continued
