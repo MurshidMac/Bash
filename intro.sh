2bcc49cbca07d312bc787be4b136c7d75751f5e9
MurshidMac <mh.murshidco.uk@outlook.com>
2017-08-05 23:45:24 +0530

    Echo hello world

--- intro.sh
+++ intro.sh
@@ -1,4 +1,5 @@
 # bash is all about textual based programming
 # its a new way to talk to the system you re building
 # its the most powerful form of transfering idea to a computer
-        
+#!/bin/bash
+echo Hello Murshid        
