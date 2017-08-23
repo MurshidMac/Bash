8dacb070b39cd82f2c6e36b20f601400d2702224
MurshidMac <mh.murshidco.uk@outlook.com>
2017-08-23 23:03:33 +0530

    Type of Shells in Linux

--- intro.sh
+++ intro.sh
@@ -2,4 +2,10 @@
 # its a new way to talk to the system you re building
 # its the most powerful form of transfering idea to a computer
 #!/bin/bash
+
+# Before we begin. There are couple of shells available for GNU/Linux
+# C-SHELL Z-SHELL TC-SHELL Bourne-SHELL KORN-SHELL etc
+# To find the default shell (mainly BASH) 
+# Type "echo $SHELL" without quotations
+
 echo Hello Murshid        
